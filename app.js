7187631cbbef057723f858703b3f8fdb18ea4e2a
Yuliana Montaño <yulianamarcela200@gmail.com>
2022-09-11 22:38:41 -0400

    Rendering database items in the todolist app

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+});
 
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+  name: "Welcome to your to do list",
+});
+const item2 = new Item({
+  name: "Hit the  + button to add a new item ",
+});
+const item3 = new Item({
+  name: "<----Hit this to delete an item",
+});
+const defaultItems = [item1, item2, item3];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let day = date.getDate();
-  res.render("list", { listTitle: day, newListItems: items });
+  Item.find({}, function (error, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (error) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Succes!");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
 });
 app.post("/", function (req, res) {
   let item = req.body.newItem;

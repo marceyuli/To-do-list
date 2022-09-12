b41e8b0af5e430a2588b6193f50b8b110f48aacf
Yuliana Montaño <yulianamarcela200@gmail.com>
2022-09-11 20:47:26 -0400

    git ignore added

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 
 const app = express();
-var items = [];
-var workItems = [];
+
+
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));

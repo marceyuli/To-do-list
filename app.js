2dd46f36e23408ae0219001cb41164f97af00883
Yuliana Montaño <yulianamarcela200@gmail.com>
2022-11-06 11:54:11 -0500

    Add git ignore procfile and update ports

--- app.js
+++ app.js
@@ -110,6 +110,12 @@ app.post("/delete", function (req, res) {
 app.get("/about", function (req, res) {
   res.render("about.ejs");
 });
-app.listen(3000, function () {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
